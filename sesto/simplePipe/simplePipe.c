2d5fb105e4bf8ba2e58a23aefe1457d9b73cf4ee
Sapocrate <saporitipuetro@gmail.com>
2021-11-24 21:59:38 +0100

    simplePipe implementato

--- sesto/simplePipe/simplePipe.c
+++ sesto/simplePipe/simplePipe.c
@@ -15,6 +15,8 @@ Comunicazione padre-figlio tramite pipe
 
 */
 
+#define LINE_LEN 90
+
 int main(int argc, char const *argv[]) {
   int pipeFD[2]; //fd della pipe - si scrive su 1 e si legge su 0
 
@@ -22,9 +24,28 @@ int main(int argc, char const *argv[]) {
   pipe(pipeFD);
   if(!fork()){
     //figlio
+    char line[LINE_LEN];
+    FILE *fp= fopen("input.txt", "r");
+    close(pipeFD[0]);
+    if(fp==NULL){
+      printf("error opening input.txt\n");
+      return 0;
+    }
+    while (fgets(line, LINE_LEN, fp)!= NULL){
+      write(pipeFD[1], line, LINE_LEN);
+    }
+    fclose(fp);
+    close(pipeFD[1]);
+    return 0;
   }
   else{
     //padre
+    char in[LINE_LEN];
+    close(pipeFD[1]);
+    while(read(pipeFD[0], in, LINE_LEN)){
+      printf("%s\n", in);
+    }
+    close(pipeFD[0]);
   }
   return 0;
 }
